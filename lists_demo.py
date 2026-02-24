8d54b974fadb87a76d1ccc4f6c12a35af219f2a1
Ujjwaljha008 <ujjwaljha008@gmail.com>
2026-02-24 23:25:40 +0530

    added lists_demo

---
+++ lists_demo.py
@@ -0,0 +1,17 @@
+name = "John"
+age = 20
+percent = 85.5
+
+student = ["John", 20, 85.5]     #------List are sequence, collection of any data of any data type
+
+print(type(student))
+print(student)
+
+days_of_week = ["Mon","Tue","Wed","Thur","Fri","Sat","Sun"]
+print(days_of_week)
+print(len(days_of_week)) #------Length of a list => the number of items/elements in the list
+print(days_of_week[3])
+print(f"Last day of the week is {days_of_week[6]}")
+print(f"Last day of the week is {days_of_week[-1]}")
+
+print(days_of_week[8])
