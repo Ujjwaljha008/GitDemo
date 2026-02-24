3c5ea68d383f625aebce73be736d4639552712b3
Ujjwaljha008 <ujjwaljha008@gmail.com>
2026-02-24 09:45:41 +0530

    Added Escape_sequences program

---
+++ escape_sequences.py
@@ -0,0 +1,23 @@
+# \n - new/next line
+# \t - tab
+# \\ - backslash
+# \' - inserts a single quote inside a single-quoted string
+# \"
+
+#\n
+# print("Hello everyone.\nHow are you?")
+
+#\t
+# print("John 20")
+# print("John\t20")
+
+#\\
+# print("new\old")
+# print("new\\old\nyes\\no")
+
+#\'
+# print('This is Python\'s class')
+# print("This is Python's class")
+
+#\"
+print("He says, \"we\'re learning Python\"")
