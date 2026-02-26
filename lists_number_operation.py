a7bc8ebcf24be116f8a9b211bb77cac9a7ddc054
Ujjwaljha008 <ujjwaljha008@gmail.com>
2026-02-26 12:40:28 +0530

    List programs

---
+++ lists_number_operation.py
@@ -0,0 +1,12 @@
+numbers=[10,4,-1,20,5.5,7,1]
+
+#Smallest number in the list
+#min()
+print(f"Smallest number: {min(numbers)}")
+
+#Biggest number in the list
+#max()
+print(f"Largest number: {max(numbers)}")
+
+#sum() to find the total of the numbers in the list
+print(f"Total: {sum(numbers)}")
---
+++ nested_lists.py
@@ -0,0 +1,11 @@
+#List inside a list
+l1=[5,1.5,"Python",True,None,[1,2,3],10]
+print(f"Size of the list is {len(l1)}")
+print(l1[-2])
+print(l1[-2][0])
+
+l2=[[1,2],[3,4],[5,6,[0,1]]]
+print(f"Size of the list is {len (l2)}")
+print(l2[2][2][1])
+
+
